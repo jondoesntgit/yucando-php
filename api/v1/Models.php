8a2fc00432ff3a6b23d81847ef80e27d0425ca63
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2015-10-20 13:42:58 -0400

    Created a GET name API per Corey Maynard tutorial
    Following the API that Corey Maynard presents
    [here](http://coreymaynard.com/blog/creating-a-restful-api-with-php/)
    I created an api that allows a user to get a name.
    
        /api/v1/example?apiKey=123
    
    should return a webpage that says
    
        "Your name is John Doe"

---
+++ api/v1/Models.php
@@ -0,0 +1,23 @@
+<?php
+namespace Models;
+
+class APIKey {
+    public function __construct() {
+	// TODO Constructor here
+    }
+
+    public function verifyKey($apiKey, $origin) {
+	// TODO verifyTheKey
+	return true;
+    }
+}
+
+class User {
+
+    public $name = "John Doe";
+
+    public function __construct() {
+	// TODO Constructor here
+    }
+}
+?>
--- api/v1/api.php
+++ api/v1/api.php
@@ -1,6 +1,16 @@
 <?php
+require_once 'MyAPI.php';
 
-$request = isset($_GET['request']) ? $_GET['request'] : null;
-echo $request;
+// Requests from the same server don't have a HTTP_ORIGIN header
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+    $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+} catch (Exception $e) {
+    echo json_encode(Array('error' => $e->getMessage()));
+}
  
 ?>
