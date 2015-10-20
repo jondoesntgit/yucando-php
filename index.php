c986134b9d5fa48655b793ce983f577ce4f73e47
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2015-10-20 17:01:17 -0400

    Created homepage that views tasks and punch ins
    Note: In progress

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 include_once('header.php');
-echo "You are on master development branch";
+require_once('db/Db_conn.class.php');
+
+function listPunches($punches) {
+  if (empty($punches)) return "No punches" ;
+  return $punches[0]['punch_in'];
+}
+
+$conn = new Db_conn();
+
+$tasks = $conn->get_tasks();
+echo "<table border=1>\n";
+echo "<tr><th>Name</th></tr>\n";
+foreach ($tasks as $task) {
+  $punches = $conn->get_punches($task['id']);
+  echo "<tr><td>" . $task['title'] . "</td><td>" . listPunches($punches) . "</td></tr>\n"; 
+  
+}
+
+
+echo "</table>";
 ?>
